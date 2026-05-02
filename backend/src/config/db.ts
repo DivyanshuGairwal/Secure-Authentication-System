15caadca2ee2bba4f4b08d8991b0e8d791f96731
Divyanshu <divyanshu.gairwal2004@gmail.com>
2026-05-03 02:44:20 +0530

    Remove hardcoded MongoDB connection string
    Removed hardcoded MongoDB connection string for security.

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -2,7 +2,6 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try{
-        await mongoose.connect("mongodb+srv://LenovoUser:Admin123@secure-auth-cluster.oeh7t8w.mongodb.net/secure-auth?retryWrites=true&w=majority");
         console.log("MongoDB connected");
       } catch (error) {
         console.error("DB connection error:", error);
